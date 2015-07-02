75a98abd6448bf5a18e654362923e093a14e9f79
narayanaega <narayana.ega@gmail.com>
2015-07-01 23:51:20 -0500

    third commit

--- server.js
+++ server.js
@@ -27,11 +27,11 @@ function checkIfLoggedIn(req, res, next){
       if (user) {
         // set a 'user' property on req so that the 'requireUser' middleware can check if the user is logged in
         req.user = user;
-        
+
         // Set a res.locals variable called 'user' so that it is available to every handlebars template.
         res.locals.user = user;
       }
-      
+
       next();
     });
   } else {
@@ -42,7 +42,7 @@ function checkIfLoggedIn(req, res, next){
 // Use this so we can get access to `req.body` in our posted forms.
 app.use( require('body-parser')() );
 
-// We need to use cookies for sessions, so use the cookie parser 
+// We need to use cookies for sessions, so use the cookie parser
 app.use( require('cookie-parser')() );
 
 app.use( expressSession({
@@ -66,7 +66,7 @@ app.set('view engine', 'html');
 app.get('/', function(req, res){
   var coll = mongo.collection('stmtdata');
   coll.find({}).toArray(function(err, stmtdata){
-    res.render('index.html', {stmtdata:stmtdata});  
+    res.render('index.html', {stmtdata:stmtdata});
   })
 });
 
@@ -106,33 +106,8 @@ app.get('/enterdata', function(req,res){
 app.get('/drafts', function(req,res){
 var coll = mongo.collection('stmtdata');
   coll.find({}).toArray(function(err, stmtdata){
-  var comcoll=mongo.collection('dfansdata');
-  comcoll.find({}).toArray(function(err,drafts){
-  var resArray=[]; 
-  var prblmnum;
-  var userPrnoArray=[];
-  for(var i=0,x=0;i<drafts.length;i++)
-  {
-	if(drafts[i]["username"]==req.user.username)
-	{
-		prblmnum=parseInt(drafts[i]["pno"]);
-		userPrnoArray[x]=prblmnum;
-		x++;
-	}
-  }
 
-  for(var i=0,x=0;i<stmtdata.length;i++)
-  {
-	prblmnum=parseInt(stmtdata[i]["sno"]);
-	if(userPrnoArray.indexOf(prblmnum)==-1)
-	{
-		resArray[x]=stmtdata[i];
-		x++;
-	}
-  }
-	res.render('drafts', {stmtdata:resArray});
-	});
-    
+	res.render('drafts', {stmtdata:stmtdata});
 });
 });
 
@@ -140,12 +115,14 @@ var coll = mongo.collection('stmtdata');
 app.get('/reviewdrafts', function(req,res){
 var coll = mongo.collection('dfansdata');
   coll.find().toArray(function(err, drafts){
+  var comcoll=mongo.collection('commentdata');
+  comcoll.find().toArray(function(err,comdata){
   var resArray=[];
   var prblmno,username;
   var userPnoArray=[];
   for(var i=0,x=0;i<drafts.length;i++)
   {
-	
+
 	if(drafts[i]["username"]==req.user.username)
 	{
 		prblmno=drafts[i]["pno"];
@@ -153,16 +130,31 @@ var coll = mongo.collection('dfansdata');
 		x++;
 	}
   }
+  var comment_done=0;
   for(var i=0,x=0;i<drafts.length;i++)
   {
 	prblmno=drafts[i]["pno"];
-	if(drafts[i]["username"]!=req.user.username && userPnoArray.indexOf(prblmno)!=-1)
+	if(drafts[i]["username"]!=req.user.username && parseInt(drafts[i]["commentcount"])<3 && userPnoArray.indexOf(prblmno)!=-1)
 	{
-		resArray[x]=drafts[i];
-		x++;
+		for(var k=0,y=0;k<comdata.length;k++)
+  		{
+
+  			if(comdata[k]["commentby"]==req.user.username && drafts[i]["dno"]==comdata[k]["dno"])
+  			{
+  				comment_done=1;
+  			}
+  		}
+  		if(comment_done==0)
+  		{
+			resArray[x]=drafts[i];
+			x++;
+		}
+		comment_done=0;
 	}
   }
+
   res.render('reviewdrafts', {drafts:resArray});
+  });
 });
 });
 
@@ -177,13 +169,11 @@ var coll = mongo.collection('dfansdata');
 //comment on a draft
 app.get('/commentondraft', function(req,res){
 var coll = mongo.collection('dfansdata');
-var dno=parseInt(req.session.dvalue);
-  coll.findOne({dno:dno}, function(err, draft){
-  var comcoll=mongo.collection('commentdata');
-  var ddno=parseInt(draft.dno);
-  comcoll.find({dno : ddno}).toArray(function(err,comdata){
-	res.render('commentondraft', {draft:draft,comdata:comdata});
-	});
+var ddno=parseInt(req.session.dvalue);
+  coll.findOne({dno:ddno}, function(err, draft){
+
+	res.render('commentondraft', {draft:draft});
+
 });
 });
 
@@ -192,9 +182,21 @@ app.get('/inputdraft', function(req,res){
 var coll = mongo.collection('stmtdata');
 var stmtno=parseInt(req.session.value);
 coll.findOne({sno: stmtno}, function(err, document) {
-  res.render('inputdraft', {document:document});
+  var draft_Collection=mongo.collection('dfansdata');
+  draft_Collection.findOne({pno:req.session.value,username:req.user.username},function(err, record){
+    var comment_Collection=mongo.collection('commentdata');
+    comment_Collection.find({dno:""+record.dno}).toArray(function(err, comments){
+      var fans_exist=false,final=false;
+      if(comments.length==3)
+      final=true;
+      if(record.finalans!="NA")
+        fans_exist=true;
+  res.render('inputdraft', {document:document,record:record,comments:comments,final:final,fans_exist:fans_exist});
 });
- 
+});
+
+});
+
 });
 
 
@@ -219,7 +221,7 @@ function createHash(string){
 //Function to created a new user
 function createUser(firstname, lastname, username, password, password_confirmation, email, userrole, callback){
   var coll = mongo.collection('users');
-  
+
   if (password !== password_confirmation) {
     var err = 'The passwords do not match';
     callback(err);
@@ -236,9 +238,9 @@ function createUser(firstname, lastname, username, password, password_confirmati
       salt: salt,
       hashedPassword: hashedPassword,
     };
-	
 
-    
+
+
     // make sure this username does not exist already
     coll.findOne(query, function(err, user){
       if (user) {
@@ -264,16 +266,16 @@ app.post('/signup', function(req, res){
   var password_confirmation = req.body.password_confirmation;
   var email = req.body.email;
    var userrole = req.body.userrole;
-  
+
   createUser(firstname, lastname,username, password, password_confirmation, email, userrole, function(err, user){
     if (err) {
       res.render('signup', {error: err});
     } else {
-      
+
       // This way subsequent requests will know the user is logged in.
       req.session.username = user.username;
-      
-      res.redirect('/');  
+
+      res.redirect('/');
     }
   });
 });
@@ -283,29 +285,32 @@ app.post('/inputdraft', function(req, res){
   // The 3 variables below all come from the form
   // in views/drafts.html
      var username=req.user.username;
-  
+
       // This way subsequent requests will know the user is logged in.
       req.session.username = username;
 	  req.session.value=req.body.selected;
-      res.redirect('/inputdraft');  
+      res.redirect('/inputdraft');
 });
 app.post('/commentondraft', function(req, res){
   // The 3 variables below all come from the form
   // in views/drafts.html
      var username=req.user.username;
-  
+
       // This way subsequent requests will know the user is logged in.
       req.session.username = username;
 	  req.session.dvalue=req.body.dselected;
-      res.redirect('/commentondraft');  
+      res.redirect('/commentondraft');
 });
 
 //function to save a draft submitted by the user
-function saveDraft(username, pno, statement, solution, callback){
-  
+function saveDraft(username, pno, statement, solution, finalsolution, callback){
+
   var coll = mongo.collection('dfansdata');
-  
-  coll.count(function(err, count) {
+
+coll.find({username:username,pno:pno}).toArray(function(err,draftdata){
+  if(draftdata.length!=1)
+  {
+  coll.count(function(err, count){
         assert.equal(null, err);
 		++count;
 		var draftObject = {
@@ -317,18 +322,26 @@ function saveDraft(username, pno, statement, solution, callback){
 	  finalans : "NA",
 	  fgcount : 0,
 	  commentcount : 0
-	  
+
     };
-    
-    
-        // insert drafts into database 
+
+
+        // insert drafts into database
         coll.insert(draftObject, function(err,user){
           callback(err,user);
         });
-	});     
+
+      });
+    }
+    else {
+            coll.update({username:username,pno:pno},{$set:{finalans:finalsolution}},function(err,user){
+              callback(err,user);
+            });
+    }
+	});
   }
-  
-  
+
+
 app.post('/savedraft', function(req, res){
   // The 3 variables below all come from the form
   // in views/drafts.html
@@ -336,55 +349,56 @@ app.post('/savedraft', function(req, res){
   var solution=req.body.solution;
   var statement=req.body.statement;
   var pno=req.session.value;
-     saveDraft(username, pno, statement, solution, function(err, user){
+  var finalsolution=req.body.finalsolution;
+     saveDraft(username, pno, statement, solution, finalsolution, function(err, user){
     if (err) {
       res.render('inputdraft', {error: err});
-    } else { 
+    } else {
 
       // This way subsequent requests will know the user is logged in.
       req.session.username = username;
-	  
-      res.redirect('/userscreen');  
-	  
+
+      res.redirect('/userscreen');
+
     }
   });
-  
-	  
+
+
 });
 
 //function to save a comment submitted by the user
 function saveComment(comment, dno, commenton, commentby, callback){
-  
+
   var coll = mongo.collection('commentdata');
-  
-  
+
+
 		var commentObject = {
 	  comment:comment,
       dno: dno,
 	  commenton: commenton,
 	  commentby:commentby,
-        
+
     };
-    
-    
-        // insert drafts into database 
+
+
+        // insert drafts into database
         coll.insert(commentObject, function(err,user){
           callback(err,user);
-        });   
+        });
 		var collection = mongo.collection('dfansdata');
 	collection.update({dno: parseInt(dno)},{$inc:{commentcount:1}}, function(err, object) {
 																					if (err){
 																								console.warn(err.message);  // returns error if no matching object found
 																							}else{
-																									
-											
+
+
 																									}
   });
   }
 app.post('/savecomment', function(req, res){
   // The 3 variables below all come from the form
   // in views/drafts.html
-  
+
   var commentby=req.user.username;
   var comment=req.body.comment;
   var commenton=req.body.commenton;
@@ -392,23 +406,23 @@ app.post('/savecomment', function(req, res){
      saveComment(comment, dno, commenton, commentby, function(err, user){
     if (err) {
       res.render('commentondraft', {error: err});
-    } else { 
+    } else {
 
       // This way subsequent requests will know the user is logged in.
       req.session.username = req.user.username;
-	  
-      res.redirect('/userscreen');  
-	  
+
+      res.redirect('/userscreen');
+
     }
   });
-  
-	  
+
+
 });
 
 //Function to input problems
 function enterData(statement, hint, solution, callback){
   var coll = mongo.collection('stmtdata');
-  
+
   coll.count(function(err, count) {
         assert.equal(null, err);
 		++count;
@@ -418,13 +432,13 @@ function enterData(statement, hint, solution, callback){
 	  hint: hint,
       solution: solution
     };
-    
-    
-        // create the new Problem 
+
+
+        // create the new Problem
         coll.insert(stmtObject, function(err,user){
           callback(err,user);
         });
-	});     
+	});
   }
 
 
@@ -435,16 +449,16 @@ app.post('/enterdata', function(req, res){
   var hint = req.body.hint;
   var solution = req.body.solution;
 
-  
+
   enterData(statement, hint, solution, function(err, user){
     if (err) {
       res.render('enterdata', {error: err});
     } else {
-      
+
       // This way subsequent requests will know the user is logged in.
       req.session.username = user.username;
-      
-      res.redirect('/');  
+
+      res.redirect('/');
     }
   });
 });
@@ -455,7 +469,7 @@ app.post('/enterdata', function(req, res){
 // This finds a user matching the username and password that were given.
 function authenticateUser(username, password, callback){
   var coll = mongo.collection('users');
-  
+
   coll.findOne({username: username}, function(err, user){
     if (err) {
       return callback(err, null);
@@ -478,7 +492,7 @@ app.post('/login', function(req, res){
   // the views/login.hbs page
   var username = req.body.username;
   var password = req.body.password;
-  
+
   authenticateUser(username, password, function(err, user){
     if(user.userrole==='admin')
 	{
@@ -504,7 +518,5 @@ mongo.connect(mongoUrl, function(){
   console.log('Connected to mongo at: ' + mongoUrl);
   app.listen(port, function(){
     console.log('Server is listening on port: '+port);
-  });  
+  });
 })
-
-
