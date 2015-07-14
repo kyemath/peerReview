f7862f438c6552076cfc5579e79ee38eb78d232c
narayanaega <narayana.ega@gmail.com>
2015-07-14 16:57:31 -0500

    Commit 2 on 14th july

--- server.js
+++ server.js
@@ -5,39 +5,12 @@ var expressSession = require('express-session');
 var expressHbs = require('express3-handlebars');
 var mongoUrl = 'mongodb://localhost:27017/regdata';
 var MongoStore = require('connect-mongo')(expressSession);
-var mongo = require('./mongo');
+var mongo = require('./lib/mongo');
 assert = require('assert');
+var methods = require('./lib/methods');
+
 var port = 3333; // port number which we are using for node server
 
-
-// to check whether the user is looged in or not while giving access to confidential pages
-function requireUser(req, res, next){
-  if (!req.user) {
-    res.redirect('/not_allowed');
-  } else {
-    next();
-  }
-}
-
-// This middleware checks if the user is logged in and sets req.user and res.locals.user appropriately if so.
-function checkIfLoggedIn(req, res, next){
-  if (req.session.username) {
-    var coll = mongo.collection('users');
-    coll.findOne({username: req.session.username}, function(err, user){
-      if (user) {
-        // set a 'user' property on req so that the 'requireUser' middleware can check if the user is logged in
-        req.user = user;
-
-        // Set a res.locals variable called 'user' so that it is available to every handlebars template.
-        res.locals.user = user;
-      }
-
-      next();
-    });
-  } else {
-    next();
-  }
-}
 
 // Use this so we can get access to `req.body` in our posted forms.
 app.use( require('body-parser')() );
@@ -55,7 +28,7 @@ app.use( expressSession({
 // We must use this middleware _after_ the expressSession middleware,
 // because checkIfLoggedIn checks the `req.session.username` value,
 // which will not be available until after the session middleware runs.
-app.use(checkIfLoggedIn);
+app.use(methods.checkIfLoggedIn);
 
 
 //set up of view engine
@@ -87,7 +60,7 @@ app.get('/not_allowed', function(req, res){
 });
 
 // The secret url includes the requireUser middleware.
-app.get('/secret', requireUser, function(req, res){
+app.get('/secret', methods.requireUser, function(req, res){
   res.render('secret');
 });
 
@@ -95,6 +68,12 @@ app.get('/secret', requireUser, function(req, res){
 app.get('/signup', function(req,res){
   res.render('signup');
 });
+
+//To get login page
+app.get('/success', function(req, res){
+  res.render('success',{layout:false});
+});
+
 
 app.get('/selectmodule', function(req, res){
   res.render('selectmodule',{layout:false});
@@ -285,56 +264,6 @@ var coll = mongo.collection('stmtdata');
 });
 });
 
-//function to create password salt
-function createSalt(){
-  var crypto = require('crypto');
-  return crypto.randomBytes(32).toString('hex');
-}
-
-//Function to create password hash
-function createHash(string){
-  var crypto = require('crypto');
-  return crypto.createHash('sha256').update(string).digest('hex');
-}
-
-
-//Function to created a new user
-function createUser(firstname, lastname, username, password, password_confirmation, email, userrole, callback){
-  var coll = mongo.collection('users');
-
-  if (password !== password_confirmation) {
-    var err = 'The passwords do not match';
-    callback(err);
-  } else {
-    var query      = {username:username};
-    var salt       = createSalt();
-    var hashedPassword = createHash(password + salt);
-    var userObject = {
-	  firstname: firstname,
-	  lastname: lastname,
-      username: username,
-	  email: email,
-	  userrole: userrole,
-      salt: salt,
-      hashedPassword: hashedPassword,
-    };
-
-
-
-    // make sure this username does not exist already
-    coll.findOne(query, function(err, user){
-      if (user) {
-        err = 'The username you entered already exists';
-        callback(err);
-      } else {
-        // create the new user
-        coll.insert(userObject, function(err,user){
-          callback(err,user);
-        });
-      }
-    });
-  }
-}
 
 //To get the data from signup form
 app.post('/signup', function(req, res){
@@ -347,7 +276,7 @@ app.post('/signup', function(req, res){
   var email = req.body.email;
    var userrole = req.body.userrole;
 
-  createUser(firstname, lastname,username, password, password_confirmation, email, userrole, function(err, user){
+  methods.createUser(firstname, lastname,username, password, password_confirmation, email, userrole, function(err, user){
     if (err) {
       res.render('signup', {error: err});
     } else {
@@ -380,7 +309,7 @@ app.post('/drafts', function(req, res){
       // This way subsequent requests will know the user is logged in.
       req.session.username = username;
 	  req.session.modno=req.body.moduleno;
-    
+
       res.redirect('/drafts');
 });
 app.post('/commentondraft', function(req, res){
@@ -403,45 +332,6 @@ app.post('/gradeonanswer', function(req, res){
 	  req.session.dvalue=req.body.dselected;
       res.redirect('/gradeonanswer');
 });
-//function to save a draft submitted by the user
-function saveDraft(username, pno, statement, solution, finalsolution, callback){
-
-  var coll = mongo.collection('dfansdata');
-
-coll.find({username:username,pno:pno}).toArray(function(err,draftdata){
-  if(draftdata.length!=1)
-  {
-  coll.count(function(err, count){
-        assert.equal(null, err);
-		++count;
-		var draftObject = {
-	  dno:count,
-      username: username,
-	  pno: pno,
-	  statement:statement,
-      draft: solution,
-	  finalans : "NA",
-	  fgcount : 0,
-	  commentcount : 0
-
-    };
-
-
-        // insert drafts into database
-        coll.insert(draftObject, function(err,user){
-          callback(err,user);
-        });
-
-      });
-    }
-    else {
-            coll.update({username:username,pno:pno},{$set:{finalans:finalsolution}},function(err,user){
-              callback(err,user);
-            });
-    }
-	});
-  }
-
 
 app.post('/savedraft', function(req, res){
   // The 3 variables below all come from the form
@@ -451,7 +341,7 @@ app.post('/savedraft', function(req, res){
   var statement=req.body.statement;
   var pno=req.session.value;
   var finalsolution=req.body.finalsolution;
-     saveDraft(username, pno, statement, solution, finalsolution, function(err, user){
+     methods.saveDraft(username, pno, statement, solution, finalsolution, function(err, user){
     if (err) {
       res.render('inputdraft', {error: err});
     } else {
@@ -459,7 +349,7 @@ app.post('/savedraft', function(req, res){
       // This way subsequent requests will know the user is logged in.
       req.session.username = username;
 
-      res.render('selectmodule',{layout:false});
+      res.render('success',{layout:false});
 
     }
   });
@@ -467,35 +357,6 @@ app.post('/savedraft', function(req, res){
 
 });
 
-//function to save a comment submitted by the user
-function saveComment(comment, dno, commenton, commentby, callback){
-
-  var coll = mongo.collection('commentdata');
-
-
-		var commentObject = {
-	  comment:comment,
-      dno: dno,
-	  commenton: commenton,
-	  commentby:commentby,
-
-    };
-
-
-        // insert drafts into database
-        coll.insert(commentObject, function(err,user){
-          callback(err,user);
-        });
-		var collection = mongo.collection('dfansdata');
-	collection.update({dno: parseInt(dno)},{$inc:{commentcount:1}}, function(err, object) {
-																					if (err){
-																								console.warn(err.message);  // returns error if no matching object found
-																							}else{
-
-
-																									}
-  });
-  }
 app.post('/savecomment', function(req, res){
   // The 3 variables below all come from the form
   // in views/drafts.html
@@ -504,7 +365,7 @@ app.post('/savecomment', function(req, res){
   var comment=req.body.comment;
   var commenton=req.body.commenton;
   var dno=req.body.dno;
-     saveComment(comment, dno, commenton, commentby, function(err, user){
+     methods.saveComment(comment, dno, commenton, commentby, function(err, user){
     if (err) {
       res.render('commentondraft', {error: err});
     } else {
@@ -512,44 +373,14 @@ app.post('/savecomment', function(req, res){
       // This way subsequent requests will know the user is logged in.
       req.session.username = req.user.username;
 
-      res.render('reviewdrafts',{layout:false});
+      res.render('success',{layout:false});
 
     }
   });
 
 
 });
-//function to save a comment submitted by the user
-function saveGrade(feedback, dno, gradeon, gradeby, grade, callback){
 
-  var coll = mongo.collection('gradedata');
-
-
-		var gradeObject = {
-	  grade:parseInt(grade),
-    feedback:feedback,
-      dno: dno,
-	  gradeon: gradeon,
-	  gradeby:gradeby
-
-
-    };
-
-
-        // insert drafts into database
-        coll.insert(gradeObject, function(err,user){
-          callback(err,user);
-        });
-		var collection = mongo.collection('dfansdata');
-	collection.update({dno:parseInt(dno)},{$inc:{fgcount:1}}, function(err, object) {
-																					if (err){
-																								console.warn(err.message);  // returns error if no matching object found
-																							}else{
-
-
-																									}
-  });
-  }
 app.post('/savegrade', function(req, res){
   // The 3 variables below all come from the form
   // in views/drafts.html
@@ -559,7 +390,7 @@ app.post('/savegrade', function(req, res){
   var gradeon=req.body.gradeon;
   var dno=req.body.dno;
   var grade=req.body.grade;
-     saveGrade(feedback, dno, gradeon, gradeby,grade, function(err, user){
+     methods.saveGrade(feedback, dno, gradeon, gradeby,grade, function(err, user){
     if (err) {
       res.render('gradeonanswer', {error: err});
     } else {
@@ -567,36 +398,13 @@ app.post('/savegrade', function(req, res){
       // This way subsequent requests will know the user is logged in.
       req.session.username = req.user.username;
 
-      res.render('gradefinal',{layout:false});
+      res.render('success',{layout:false});
 
     }
   });
 
 
 });
-
-//Function to input problems
-function enterData(modno,statement, hint, solution, callback){
-  var coll = mongo.collection('stmtdata');
-
-  coll.count(function(err, count) {
-        assert.equal(null, err);
-		++count;
-		var stmtObject = {
-	  sno:count,
-	  modno:modno,
-      statement: statement,
-	  hint: hint,
-      solution: solution
-    };
-
-
-        // create the new Problem
-        coll.insert(stmtObject, function(err,user){
-          callback(err,user);
-        });
-	});
-  }
 
 
 app.post('/enterdata', function(req, res){
@@ -608,7 +416,7 @@ app.post('/enterdata', function(req, res){
   var modno=req.body.moduleno;
 
 
-  enterData(modno, statement, hint, solution, function(err, user){
+  methods.enterData(modno, statement, hint, solution, function(err, user){
     if (err) {
       res.render('enterdata', {error: err});
     } else {
@@ -621,37 +429,13 @@ app.post('/enterdata', function(req, res){
   });
 });
 
-
-
-
-// This finds a user matching the username and password that were given.
-function authenticateUser(username, password, callback){
-  var coll = mongo.collection('users');
-
-  coll.findOne({username: username}, function(err, user){
-    if (err) {
-      return callback(err, null);
-    }
-    if (!user) {
-      return callback(null, null);
-    }
-    var salt = user.salt;
-    var hash = createHash(password + salt);
-    if (hash === user.hashedPassword) {
-      return callback(null, user);
-    } else {
-      return callback(null, null);
-    }
-  });
-}
-
 app.post('/login', function(req, res){
   // These two variables come from the form on
   // the views/login.hbs page
   var username = req.body.username;
   var password = req.body.password;
 
-  authenticateUser(username, password, function(err, user){
+  methods.authenticateUser(username, password, function(err, user){
     if(user)
     {
       if(user.userrole==='admin')
